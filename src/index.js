3e165d2faacc2f2853adfd24d612880be5be186e
Teemi2106 <teemiofficial13@gmail.com>
2024-12-02 17:23:59 +0100

    "Added ScrollToTop component to BrowserRouter in index.js"

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
